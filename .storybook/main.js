548470c94ea01c8c4803e4c7b11c4d1954283505
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-08-29 12:34:37 +0530

    fix sass plugin version conflict

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 module.exports = {
   stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
@@ -10,7 +12,7 @@ module.exports = {
     config.module.rules.push({
       test: /\.scss$/,
       use: ["style-loader", "css-loader", "sass-loader"],
-      include: path.resolve(__dirname, "../src"),
+      include: path.resolve(__dirname, "../src/"),
     });
 
     return config;
