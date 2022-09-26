b060db1dc2e1d628db784a7f7886134ecbef104a
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-09-26 12:37:01 +0530

    button: remove zeplin links

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -5,6 +5,7 @@ import { Button } from './Button';
 export default {
 	title: 'Atoms/Button',
 	component: Button,
+	parameters: {},
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
@@ -14,18 +15,10 @@ Primary.args = {
 	label: 'Button',
 	primary: true,
 };
-Primary.parameters = {
-	zeplinLink:
-		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c38290bbf15ecde8564',
-};
 
 export const Secondary = Template.bind({});
 Secondary.args = {
 	label: 'Button',
-};
-Secondary.parameters = {
-	zeplinLink:
-		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c3807a6451582cab68d',
 };
 
 export const Medium = Template.bind({});
@@ -33,17 +26,9 @@ Medium.args = {
 	label: 'Button',
 	size: 'medium',
 };
-Medium.parameters = {
-	zeplinLink:
-		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c3807a6451582cab68d',
-};
 
 export const Large = Template.bind({});
 Large.args = {
 	label: 'Button',
 	size: 'large',
-};
-Large.parameters = {
-	zeplinLink:
-		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c38c849561b214b5b16',
 };
