c5577996ff3b7fbd490003fefe504440ad102515
BlazeIsClone <64116737+BlazeIsClone@users.noreply.github.com>
2022-09-26 12:32:02 +0530

    button: add sub zeplin links

--- src/components/button/Button.stories.tsx
+++ src/components/button/Button.stories.tsx
@@ -5,10 +5,6 @@ import { Button } from './Button';
 export default {
 	title: 'Atoms/Button',
 	component: Button,
-	parameters: {
-		zeplinLink:
-			'zpl://components?pid=63314bec5c936a148ce98762&cseid=63314c33face09148172f2b8',
-	},
 } as ComponentMeta<typeof Button>;
 
 const Template: ComponentStory<typeof Button> = args => <Button {...args} />;
@@ -18,10 +14,18 @@ Primary.args = {
 	label: 'Button',
 	primary: true,
 };
+Primary.parameters = {
+	zeplinLink:
+		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c38290bbf15ecde8564',
+};
 
 export const Secondary = Template.bind({});
 Secondary.args = {
 	label: 'Button',
+};
+Secondary.parameters = {
+	zeplinLink:
+		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c3807a6451582cab68d',
 };
 
 export const Medium = Template.bind({});
@@ -29,9 +33,17 @@ Medium.args = {
 	label: 'Button',
 	size: 'medium',
 };
+Medium.parameters = {
+	zeplinLink:
+		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c3807a6451582cab68d',
+};
 
 export const Large = Template.bind({});
 Large.args = {
 	label: 'Button',
 	size: 'large',
+};
+Large.parameters = {
+	zeplinLink:
+		'zpl://components?pid=63314bec5c936a148ce98762&coid=63314c38c849561b214b5b16',
 };
